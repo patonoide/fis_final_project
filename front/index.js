05db49cc68c6be54c3f58df7b1fa3cfc135ab1a6
Suniaster <thibousbr@gmail.com>
2021-11-01 18:19:34 -0300

    test2

--- front/index.js
+++ front/index.js
@@ -5,17 +5,18 @@ async function doupload() {
   formData.append("file", photo);
   formData.append("type", 1);
 
-  fetch('localhost:5000/furry', {method: "POST", body: formData}).then( async (response) => {
+  document.getElementById("list-results").innerHTML = "";
+
+  fetch('http://127.0.0.0:5000/furry', {method: "POST", body: formData}).then( async (response) => {
       let data = await response.json();
-      document.getElementById("#list-results").innerHTML = "";
       data.forEach((result)=> {
-        document.getElementById("#list-results").innerHTML += ` 
+        document.getElementById("list-results").innerHTML += ` 
         <li class="nav-item">
           <a class="nav-link active show" data-bs-toggle="tab">
             <h4>Image: ` + result.name + `</h4>
             <p>We found that this image is ` + result.value*100 + `% furry.</p>
           </a>
-        </li>`
+        </li>`;
       })
       window.scrollTo(0,document.body.scrollHeight);
   });
